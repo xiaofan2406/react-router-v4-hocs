c0c091a7387bb645cae0b26306a0c9eb77e82a26
Xiaofan Wu <Xiaofan2406@gmail.com>
2016-11-04 10:56:07 +1100

    babel compile file

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-import WithRouter from './withRouter';
-import WithLocation from './withLocation';
-
-
-export const withRouter = WithRouter;
-export const withLocation = WithLocation;
+exports.withRouter = require('./lib/withRouter');
+exports.withLocation = require('./lib/withLocation');
